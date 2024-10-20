d83fc7cc8b26ab4a4c6cd41f141a3778ba62402d
somnath <somjsr2019@gmail.com>
2024-10-20 15:29:44 +0530

    Updated index.js for server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require('express')
-const dotenv = require('dotenv').config();
+const dotenev = require('dotenv').config()
 const Dbconnect = require('./config/Dbconnection')
+const router = require('./routes/Userroutes')
+const Taxrate = require('./controller/Usercontroller')
+const cors = require('cors')
 const app = express();
 
 //Db connection
 Dbconnect();
 app.use(express.json());
+app.use('/api/v1',router);
+app.use(cors());
+
 
 const port = process.env.PORT;
-app.listen(port, (req, res) => {
-    console.log(`Server Listenning at port ${port}`);
+
+
+
+app.listen(port , (req,res)=>{
+    console.log(`Server Listenning at Port ${port}`);
 })
