c37cb76962e9bced71553111f0ba1563900a8db3
somnath <somjsr2019@gmail.com>
2024-10-20 14:53:52 +0530

    Add template

---
+++ client/src/components/FinanceAssistant.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FinanceAssistant = () => {
+  return (
+    <div>FinanceAssistant</div>
+  )
+}
+
+export default FinanceAssistant
---
+++ client/src/components/TaxCalculation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TaxCalculation = () => {
+  return (
+    <div>TaxCalculation</div>
+  )
+}
+
+export default TaxCalculation
